e07708dc08b7ecc54007f47187bda9358414431d
luowei <luowei@wodedata.com>
2019-12-12 22:35:32 +0800

    添加脚本

---
+++ package.sh
@@ -0,0 +1,5 @@
+# 指定Name
+LibName="LWPrettyText"
+
+pod package ${LibName}.podspec --force --no-mangle --configuration=Release
+
---
+++ pub_podspec.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+pod repo push mygitlabrepo LWPrettyText.podspec --verbose --allow-warnings --sources="https://github.com/CocoaPods/Specs.git"
